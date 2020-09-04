e9e23013d207c0a3050bce120b5168d85e823db7
HirakN <hiraknaik.hn@gmail.com>
2020-09-04 12:59:58 +0100

    change server start message to use dyanmic port variable

--- src/app.js
+++ src/app.js
@@ -100,5 +100,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Server is up.\nhttp://localhost:3000')
+    console.log(`Server is up.\nhttp://localhost:${port}`)
 })

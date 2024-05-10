3e070744329a5bb0c42def5e7baabd0f890ad29b
HirakN <hiraknaik.hn@gmail.com>
2024-05-10 21:13:13 +0100

    updated to use compose secrets OR env var

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,10 +1,11 @@
 const request = require('request')
+const fs = require('fs')
 
 const forecast = (lat, long, callback) => {
-    const access_key = process.env.WEATHERSTACK_KEY
-    const url= 'http://api.weatherstack.com/current?access_key=' + access_key + '&query=' + lat + ',' + long
+    const access_key = process.env.WEATHERSTACK_KEY || fs.readFileSync(process.env.WEATHERSTACK_KEY_FILE, { encoding: 'utf-8' })
+    const url = 'http://api.weatherstack.com/current?access_key=' + access_key + '&query=' + lat + ',' + long
 
-    request({url, json: true}, (error, { body }) => {
+    request({ url, json: true }, (error, { body }) => {
         console.log(body)
         if (error) {
             callback('Unable to connect to weatherstack forecast services.', undefined, undefined)
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,14 +1,15 @@
 const request = require('request')
+const fs = require('fs')
 
 const geoCode = (address, callback) => {
-    const mapbox_token = process.env['MAPBOX_TOKEN']
+    const mapbox_token = process.env['MAPBOX_TOKEN'] || fs.readFileSync(process.env.MAPBOX_TOKEN_FILE, { encoding: 'utf-8' })
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=" + mapbox_token + "&limit=1"
-    
-    request({url: url, json: true}, (error, { body }) => {
+
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to mapbox location services.', undefined)
         } else if (body?.message) {
-            callback('mapbox api: '+body.message, undefined)
+            callback('mapbox api: ' + body.message, undefined)
         } else if (body?.features?.length === 0) {
             callback('Unable to find location, Try another search', undefined)
         } else {
